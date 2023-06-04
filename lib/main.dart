8894b8da326d43ad5dff6864ac5c861cb0670a29
TheLastFlame <thelastsight@mail.ru>
2023-06-04 20:04:28 +1000

    add i18n and full screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:thelastlife/routes.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'package:flutter_web_plugins/url_strategy.dart';
+
+import 'i18n/strings.g.dart';
+
+Future<void> main() async {
+  usePathUrlStrategy();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  // settingsData!.lang == null
+  //     ? LocaleSettings.useDeviceLocale()
+  //     : LocaleSettings.setLocale(AppLocale.values.singleWhere(
+  //         (element) => element.languageCode == settingsData?.lang));
+  LocaleSettings.useDeviceLocale();
+
+  settingUpSystemUIOverlay();
+
+  runApp(TranslationProvider(child: const MyApp()));
 }
+
+void settingUpSystemUIOverlay() {
+// Setting SysemUIOverlay
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      systemStatusBarContrastEnforced: false,
+      systemNavigationBarColor: Colors.transparent,
+      systemNavigationBarDividerColor: Colors.transparent));
+// Setting SystmeUIMode
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,6 +40,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      locale:
+            TranslationProvider.of(context).flutterLocale, // use provideraQ  8
+      supportedLocales: AppLocaleUtils.supportedLocales,
+      localizationsDelegates: GlobalMaterialLocalizations.delegates,
+
       theme: ThemeData(useMaterial3: true),
       darkTheme: ThemeData.dark(useMaterial3: true),
       routerConfig: router,
