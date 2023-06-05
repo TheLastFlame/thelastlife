0f526ab946acec2e5fde8a16f8c0c6d6e1112b72
azpantic <viktoralekseev1999@gmail.com>
2023-06-05 16:13:47 +1000

    add event

--- lib/story/actions.dart
+++ lib/story/actions.dart
@@ -4,7 +4,13 @@ var allActions = [
   ActionModel(id: 0, addEventId: 1, name: 'Проснуться'),
   ActionModel(id: 1, addEventId: 1, name: 'Всё равно проснуться'),
   ActionModel(id: 2, addEventId: 1, name: 'Ты правда думаешь, что будет выбор?'),
-
-  ActionModel(id: 0, addEventId: 1, name: 'Проснуться'),
-
+  ActionModel(id: 3, addEventId: 1, name: 'Идти'),
+  ActionModel(id: 4, addEventId: 1, name: 'Не идти'),
+  ActionModel(id: 5, addEventId: 1, name: 'Учится'),
+  ActionModel(id: 6, addEventId: 1, name: 'Отдохнуть'),
+  ActionModel(id: 7, addEventId: 1, name: 'Сесть за задания'),
+  ActionModel(id: 8, addEventId: 1, name: 'Проигнорировать'),
+  ActionModel(id: 9, addEventId: 1, name: 'Готовиться'),
+  ActionModel(id: 10, addEventId: 1, name: 'Не готовиться'),
+  ActionModel(id: 11, addEventId: 1, name: 'Списать'),
 ];
--- lib/story/events.dart
+++ lib/story/events.dart
@@ -1,13 +1,53 @@
-
-
 import 'package:thelastlife/story/actions.dart';
 
 import '../controllers/game.dart';
 
 var allEvents = [
-      Event(id: 1, name: 'test', description:  'hi', actions: [
-        allActions.firstWhere((element) => element.id == 0),
-        allActions.firstWhere((element) => element.id == 1),
-        allActions.firstWhere((element) => element.id == 2)
-      ], )
+  Event(
+    id: 1,
+    name: 'test',
+    description: 'hi',
+    actions: [
+      allActions.firstWhere((element) => element.id == 0),
+      allActions.firstWhere((element) => element.id == 1),
+      allActions.firstWhere((element) => element.id == 2)
+    ],
+  ),
+  Event(
+    id: 2,
+    name: 'Пара',
+    description: 'У вас сейчас проходит занятие',
+    actions: [
+      allActions.firstWhere((element) => element.id == 3),
+      allActions.firstWhere((element) => element.id == 4),
+    ],
+  ),
+  Event(
+    id: 3,
+    name: 'Свободное время',
+    description: 'Хмм появилось свободное время',
+    actions: [
+      allActions.firstWhere((element) => element.id == 5),
+      allActions.firstWhere((element) => element.id == 6),
+    ],
+  ),
+  Event(
+    id: 4,
+    name: 'Домашнее задание',
+    description: 'Кажется вас задали домашнее задание в универе',
+    actions: [
+      allActions.firstWhere((element) => element.id == 7),
+      allActions.firstWhere((element) => element.id == 8),
+    ],
+  ),
+  Event(
+    id: 5,
+    name: 'Контрольная работа',
+    description: 'Скоро будет контрольная работа',
+    actions: [
+      allActions.firstWhere((element) => element.id == 9),
+      allActions.firstWhere((element) => element.id == 10),
+      allActions.firstWhere((element) => element.id == 11),
+    ],
+  ),
 ];
